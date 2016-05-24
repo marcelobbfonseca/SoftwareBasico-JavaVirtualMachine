a5c4ee82da872a35932e621eb0bc4c37a60da07b
Francisco Anderson Bezerra Rodrigues <anders1232@mail.com>
2016-05-24 16:58:03 -0300

    Feito limpeza de alguns arquivos.

---
+++ include/Atributos.hpp
@@ -0,0 +1,59 @@
+#include<stdint.h>
+#include<stdio.h>
+
+namespace Atributos
+{
+	class ConstantValue_attribute
+	{
+		private:
+			uint16_t attribute_name_index;
+			uint32_t attribute_length;
+			uint16_t constantvalue_index;
+		public:
+			ConstantValue(FILE *arq);
+			void ExibirInformações(void);
+	}
+
+	class exception_element {
+		private:
+			uint16_t start_pc;
+			uint16_t end_pc;
+			uint16_t handler_pc;
+			uint16_t catch_type;
+		public:
+		exception_table (FILE *arq);
+		void ExibirInformacoes(void);
+	}
+
+	class Code_attribute
+	{
+		private:
+			uint16_t attribute_name_index;
+			uint32_t attribute_length;
+			uint16_t max_stack;
+			uint16_t max_locals;
+			uint32_t code_length;
+			uint8_t code[code_length];
+			uint16_t exception_table_length;
+			{
+				uint16_t start_pc;
+				uint16_t end_pc;
+				uint16_t handler_pc;
+				uint16_t catch_type;
+			} exception_table[exception_table_length];
+			uint16_t attributes_count;
+			attribute_info attributes[attributes_count];
+		public:
+			Code_attribute(FILE *arq);
+			void ExibirInformações(void);
+	}
+
+	class StackMapTable_attribute
+	{
+		public:
+			u2 attribute_name_index;
+			u4 attribute_length;
+			u2 number_of_entries;
+			stack_map_frame entries[number_of_entries];
+	}
+}
--- include/exception_table.hpp
+++ include/exception_table.hpp
@@ -1,19 +1,16 @@
 #include<stdint.h>
 #include<stdio.h>
-//#include<vector>
 
 #ifndef EXCEPTION_TABLE_HPP
 #define EXCEPTION_TABLE_HPP
 
-class exception_table {
-
-    uint16_t start_pc;
-    uint16_t end_pc;
-    uint16_t handler_pc;
-    uint16_t catch_type;
-
-        public:
-
+class exception_element {
+	private:
+		uint16_t start_pc;
+		uint16_t end_pc;
+		uint16_t handler_pc;
+		uint16_t catch_type;
+	public:
 	exception_table (FILE *arq);
 	~exception_table ();
 	void ExibirInformacoes(void);
