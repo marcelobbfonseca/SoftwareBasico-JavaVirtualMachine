b344123fb35823f7f67040c7160e87a826e11273
VitorSDDF <vitor.sd@hotmail.com>
2016-06-01 02:51:10 -0300

    e e e e

--- main.cpp
+++ main.cpp
@@ -4,22 +4,22 @@
 #include"JavaClass.hpp"
 #include"Erro.hpp"
 
-
-int main()
+int main(int argc, char **argv)
 {
-	std::string caminho= "Exemplos/Factorial.class";
-	try{
-		JavaClass *j1= new JavaClass(caminho);
-		j1->ExibirInformacoes();
-		delete j1;
-	}
-	catch(Erro* err)
-	{
-		std::cout<<err->GetMensagem()<<"\n";
-	}
-	catch(...)
-	{
-		std::cout << "[ERRO] Exceção desconhecida lançada!" << std::endl;
-	}
+        std::string caminho = argv[1];
+        try{
+            JavaClass *j1= new JavaClass(caminho);
+            j1->ExibirInformacoes();
+            delete j1;
+        }
+        catch(Erro* err)
+        {
+            std::cout<<err->GetMensagem()<<"\n";
+        }
+        catch(...)
+        {
+            std::cout << "[ERRO] Exceção desconhecida lançada!" << std::endl;
+        }
+
 	return 0;
 }
