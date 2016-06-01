f1be5bdf7cba3cecd706184cb32ce12e1aa316bb
Anders1232 <anders1232@mail.com>
2016-06-01 02:53:57 -0300

    Trocado espaço por tabs

--- main.cpp
+++ main.cpp
@@ -6,20 +6,20 @@
 
 int main(int argc, char **argv)
 {
-        std::string caminho = argv[1];
-        try{
-            JavaClass *j1= new JavaClass(caminho);
-            j1->ExibirInformacoes();
-            delete j1;
-        }
-        catch(Erro* err)
-        {
-            std::cout<<err->GetMensagem()<<"\n";
-        }
-        catch(...)
-        {
-            std::cout << "[ERRO] Exceção desconhecida lançada!" << std::endl;
-        }
+		std::string caminho = argv[1];
+		try{
+			JavaClass *j1= new JavaClass(caminho);
+			j1->ExibirInformacoes();
+			delete j1;
+		}
+		catch(Erro* err)
+		{
+			std::cout<<err->GetMensagem()<<"\n";
+		}
+		catch(...)
+		{
+			std::cout << "[ERRO] Exceção desconhecida lançada!" << std::endl;
+		}
 
 	return 0;
 }
