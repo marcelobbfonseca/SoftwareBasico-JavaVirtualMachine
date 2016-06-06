cfd6aa58da416f5686b751d04eacd3544d420981
Francisco Anderson Bezerra Rodrigues <anders1232@mail.com>
2016-06-06 20:15:01 -0300

    Criado função para ajudar ananálise dos argumentos passados ao programa e colocado a main para utilizá-lo.

--- main.cpp
+++ main.cpp
@@ -3,9 +3,38 @@
 #include<iostream>
 #include"JavaClass.hpp"
 #include"Erro.hpp"
+#include"AnalisadorArgumentos.hpp"
+
+#define JVM_NAO_FEITA
 
 int main(int argc, char **argv)
 {
+//pedido de ajuda
+	if(AnalisadorArgumentos::EncontrarArgumento("--help", argc, argv) != -1)
+	{
+//		ExibirMensagemDeAjuda();
+	}
+//Chamando apenas o exibidor
+	else if(AnalisadorArgumentos::EncontrarArgumento("--exibir-informacoes", argc, argv) != -1)
+	{
+		try{
+			JavaClass *j1= new JavaClass(caminho);
+			j1->ExibirInformacoes();
+			delete j1;
+		}
+		catch(Erro* err)
+		{
+			std::cout<<err->GetMensagem()<<"\n";
+		}
+		catch(...)
+		{
+			std::cout << "[ERRO] Exceção desconhecida lançada!" << std::endl;
+		}
+	}
+	else
+	{
+//caso queira rodar a JVM
+#ifdef JVM_NAO_FEITA
 		std::string caminho = argv[1];
 		try{
 			JavaClass *j1= new JavaClass(caminho);
@@ -20,6 +49,9 @@ int main(int argc, char **argv)
 		{
 			std::cout << "[ERRO] Exceção desconhecida lançada!" << std::endl;
 		}
-
+#else
+//falta codar aqui
+#endif
+	}
 	return 0;
 }
