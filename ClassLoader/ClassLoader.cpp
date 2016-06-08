0026193593e5a34e378e18ccac7a07e1b4d36585
Francisco Anderson Bezerra Rodrigues <anders1232@mail.com>
2016-06-08 17:57:05 -0300

    Fazendo ClassLoader...

--- ClassLoader/ClassLoader.cpp
+++ ClassLoader/ClassLoader.cpp
@@ -1,6 +1,11 @@
 
 ClassLoader::CarregarClasse(string nomeClasse)
 {
+	//Se não tiver.class no nome da classe .class é adicionado
+	if(!nomeClasse.substr(nomeClasse.length() - 6)== ".class")
+	{
+		nomeClasse+= ".class";
+	}
 	//ler a porra toda e mandar pro DataArea
 	if(!classesCarregadas.Existe(nomeClasse))
 	{
