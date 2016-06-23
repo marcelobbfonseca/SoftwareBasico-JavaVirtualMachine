4cfc24551bc722219f2c729e778319705d636b49
VitorSDDF <vitor.sd@hotmail.com>
2016-06-22 23:11:06 -0300

    Pra que eu tenho que digitar essa porra dessa mensagem?

--- RuntimeDataAreas/Heap.cpp
+++ RuntimeDataAreas/Heap.cpp
@@ -1,18 +1,1 @@
-class Heap {
 
-
-private:
-
-    Heap(Heap const&); //não permite implementação cópia
-    void operator=(Heap const&); // não permite atribuição
-
-    vector<Objeto*> vetorObjetos;
-
-public:
-
-    Heap();
-
-    void Heap::adcObjeto(Objeto *objeto) {
-        vetorObjetos.push_back(objeto);
-    }
-};
