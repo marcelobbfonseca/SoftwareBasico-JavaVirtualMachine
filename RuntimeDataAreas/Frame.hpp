673dd7d6e86a269094ab098c3ceba4d2f9532ae3
Francisco Anderson Bezerra Rodrigues <anders1232@mail.com>
2016-07-06 13:23:39 -0300

    Corrigido erro de compilação

--- RuntimeDataAreas/Frame.hpp
+++ RuntimeDataAreas/Frame.hpp
@@ -41,7 +41,7 @@ class Frame{
 		Frame(Objeto *objeto, string nomeMetodo, string descritorMetodo);
 		Frame(JavaClass *javaClass, string nomeMetodo, string descritor, RuntimeDataArea *runtimeDataArea);
 		Frame(ObjetoInstancia *objeto, string nomeDoMetodo, string descritorMetodo, vector<Valor> argumentos, RuntimeDataArea *runtimeDataArea);
-		Frame(ObjetoInstancia *objeto, JavaClass *javaClass, string nomeDoMetodo, string descritorMetodo, vector<Valor> argumentos, RuntimeDataArea *runtimeDataArea)
+		Frame(ObjetoInstancia *objeto, JavaClass *javaClass, string nomeDoMetodo, string descritorMetodo, vector<Valor> argumentos, RuntimeDataArea *runtimeDataArea);
 
 		Valor getValorVariavelLocal(uint32_t indice);
 		void mudarVariavelLocal(Valor valorDaVariavel, uint32_t indice);
